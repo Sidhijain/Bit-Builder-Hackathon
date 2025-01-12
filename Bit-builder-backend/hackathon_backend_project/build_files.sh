af1cb9351b73d12431840bc849195c9a3494d4e3
Anish Dhawalikar <anishdhawalikar@L-G57VHF94JF.local>
2025-01-13 00:17:54 +0530

    changes for deployment testing

--- Bit-builder-backend/hackathon_backend_project/build_files.sh
+++ Bit-builder-backend/hackathon_backend_project/build_files.sh
@@ -1,4 +1,14 @@
+# echo "BUILD START"
+# python3.8 -m pip install -r requirements.txt
+# python3.8 manage.py runserver
+# echo "BUILD END"
+
 echo "BUILD START"
-python3.8 -m pip install -r requirements.txt
-python3.8 manage.py runserver
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+# Collect static files
+python manage.py collectstatic --noinput
+# Run database migrations (if required)
+python manage.py migrate --noinput
 echo "BUILD END"
--- Bit-builder-backend/hackathon_backend_project/hackathon_backend_project/settings.py
+++ Bit-builder-backend/hackathon_backend_project/hackathon_backend_project/settings.py
@@ -117,14 +117,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # import os
 # STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
