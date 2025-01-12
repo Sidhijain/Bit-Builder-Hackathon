319a4324936fcc0e49a88ea9ef92fefdeab9b4f9
Sidhi Jain <sidhijain@Sidhis-MacBook-Pro.local>
2025-01-12 22:06:53 +0530

    demo-api frontend

---
+++ Bit-builder-frontend/hackfrontend/hackfrontend/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { DemoDataService } from './Services/demo-data.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'hackfrontend';
+  data: any; 
+
+  constructor(private dataService : DemoDataService) {}
+  ngOnInit() :void{
+  this.dataService.getData().subscribe(
+    (response: any) => {
+      if(response.status=="success"){
+      this.data = response.message;
+      console.log('Data fetched successfully:', response);
+      }
+    },
+    (error: any) => {
+      console.error('Error fetching data:', error);
+    }
+  );
+  }
+}
