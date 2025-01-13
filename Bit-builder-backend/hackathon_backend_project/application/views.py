b365c900cbe59c22ad343cfcc0525952bee259f0
Anish Dhawalikar <anishdhawalikar@L-G57VHF94JF.local>
2025-01-13 16:05:15 +0530

    deployment testing3

--- Bit-builder-backend/hackathon_backend_project/application/views.py
+++ Bit-builder-backend/hackathon_backend_project/application/views.py
@@ -3,12 +3,4 @@ from django.http import JsonResponse
 
 def sample_get_api(request):
     if request.method == "GET":
-        data = {
-            "message": "Hello, this is a simple GET API!",
-            "status": "success"
-        }
-        return JsonResponse(data)
-    else:
-        return JsonResponse({"error": "Invalid request method"}, status=400)
-    
-    
+        return render(request, 'hello.html')
--- Bit-builder-backend/hackathon_backend_project/hackathon_backend_project/settings.py
+++ Bit-builder-backend/hackathon_backend_project/hackathon_backend_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hackathon_backend_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-import os 
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
